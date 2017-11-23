8b9275544cceedc52ca3efb1cad0b75dae027622
Hodonou Sounton <sounton@gmail.com>
2017-11-24 00:29:18 +0100

    sounton plus

---
+++ server/boot/sample-models.js
@@ -0,0 +1,18 @@
+'use strict';
+// Copyright IBM Corp. 2015,2016. All Rights Reserved.
+// Node module: loopback-example-access-control
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
+module.exports = function(app) {
+  var User = app.models.Account;
+
+  User.create([{
+    username: 'Hodonou',
+    email: 'sounton@gmail.com',
+    password: 'opensesame'}], function(err, users) {
+    if (err) throw err;
+
+    console.log('Created users:', users);
+  });
+};
