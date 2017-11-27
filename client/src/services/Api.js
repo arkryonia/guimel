12ffef8ceaa009ba57f6c37bf02833e030458462
Hodonou Sounton <sounton@gmail.com>
2017-11-27 09:45:18 +0100

    kook

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 function getAPI() {
   if (process.env.NODE_ENV === 'production') {
-    return 'http://api.sounton.me/'
+    return 'http://api.sounton.me/api/'
   }
 
   return 'http://localhost:3000/api/'
